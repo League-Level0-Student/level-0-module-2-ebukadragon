d1a24a447695f9c8f7bb0107ec6599535f72e4a7
Student <Student@165.24.36.160>
2018-02-26 14:23:16 -0800

    i did a lot of code

--- src/random/Validation.java
+++ src/random/Validation.java
@@ -10,18 +10,20 @@ import javax.swing.JOptionPane;
 public class Validation {
 	public static void main(String[] args) {
 
-		// 1. OPTIONAL: Watch the first 2 minutes of this movie: http://vimeo.com/2485018
+		// 1. OPTIONAL: Watch the first 2 minutes of this movie: http://
 		
 		int randomNumber = new Random().nextInt(5);
-		
+		if (randomNumber==0){JOptionPane.showMessageDialog(null, "I like your shoes");  
+			
+		}
 		// 2. On paper, write all the numbers that get printed when you run this class 10 times
-		
+		if (randomNumber==1){JOptionPane.showMessageDialog(null, "I like your shoes");
 		System.out.println(randomNumber);
-
+		}
 		// 3. Use each value of randomNumber to give the user a random compliment.
-
+		if (randomNumber==2){JOptionPane.showMessageDialog(null, "I like your shoes");}
 		// 4. Repeat all the code above 10 times
-		
+		if (randomNumber==3){JOptionPane.showMessageDialog(null, "I like your shoes");}
 		// 5. Find someone to test out your program. They will like it :)
-	}
-}
+	if (randomNumber==4){JOptionPane.showMessageDialog(null, "I like your shoes");}
+}}
