588d7b278df269ae37fc96bd24229b73c44acd5d
ebukadragon <chukwuebuka.alozie@gompersstudent.org>
2018-04-16 14:28:21 -0700

    Add files via upload

---
+++ IntroToRandom.java
@@ -0,0 +1,31 @@
+//    Copyright (c) The League of Amazing Programmers 2013-2017
+//    Level 0
+
+package random;
+
+import java.util.Random;
+
+public class IntroToRandom {
+	public static void main(String[] args) {
+		Random ebuka=new Random();
+		int r = ebuka.nextInt(101);
+		r = ebuka.nextInt(21)+30; 
+		r = ebuka.nextInt(51)+25;
+		r = ebuka.nextInt(311)+-222;
+		//3. Now make r random. 
+		//   Create an object of the Random class
+		//	 use .nextInt() to get a random number
+		
+
+		//4. Limit the random number between 0 and 100`
+
+		//5. Limit the random number between 25 and 75. Hint: ((highest value - lowest value)+1) + lowest value
+
+		//6. Challenge: Limit the random number between -222 and 88
+
+		//1. Print out the value of r
+		System.out.println(r);
+		//2. Run the program.  What number appears in the console?
+		//   Run it again. Is the number the same?
+	}
+}                                                                                                                                 
---
+++ LotteryNumbers.java
@@ -0,0 +1,15 @@
+package random;
+
+public class LotteryNumbers {
+public static void main(String[] args) {
+Random ebuka = new Random();
+int r0 = ebuka.nextInt(51);	
+int r2 = ebuka.nextInt(216);
+int r3 = ebuka.nextInt(51)+50;
+int r4 = ebuka.nextInt(169)+105;
+int r = ebuka.nextInt(51);
+}
+}
+((273-105)+1)+105
+((168)+1)+105
+(169)+105
